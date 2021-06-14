19f26ea1f9e6250bd49fd4d741691a64e3ba2c83
Fredrik Johansson <jeanfredrik@gmail.com>
2021-06-14 17:49:35 +0200

    feat: improve default navigation

--- packages/gatsby-theme-wordpress-basic/src/components/HeaderFlyOutMenu.js
+++ packages/gatsby-theme-wordpress-basic/src/components/HeaderFlyOutMenu.js
@@ -1,7 +1,12 @@
-import { Icon } from "@whitespace/components";
+import { useLocation } from "@gatsbyjs/reach-router";
+import { Icon, TreeMenu } from "@whitespace/components";
+import { usePrevious } from "@whitespace/gatsby-hooks";
 import clsx from "clsx";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import usePages from "../hooks/pages";
+import { getTreeStructure } from "../utils/pageTree";
 
 import * as defaultStyles from "./HeaderFlyOutMenu.module.css";
 
@@ -15,9 +20,36 @@ export default function HeaderFlyOutMenu({
   className,
   ...restProps
 }) {
+  const location = useLocation();
+  const prevLocation = usePrevious(location);
+  const allPages = usePages();
+  const items = getTreeStructure(allPages);
+  const [expanded, setExpanded] = useState(false);
+
+  useEffect(() => {
+    if (location !== prevLocation) {
+      setExpanded(false);
+    }
+  }, [location, prevLocation]);
   return (
     <div className={clsx(styles.component, className)} {...restProps}>
-      <Icon name="menu" />
+      <button
+        type="button"
+        onClick={() => setExpanded(!expanded)}
+        className={styles.toggle}
+      >
+        <Icon name="menu" />
+      </button>
+      {expanded && (
+        <>
+          <div className={styles.backdrop} onClick={() => setExpanded(false)} />
+          <TreeMenu
+            items={items}
+            location={location}
+            className={clsx(styles.menu)}
+          />
+        </>
+      )}
     </div>
   );
 }
--- packages/gatsby-theme-wordpress-basic/src/components/SiteLayout.js
+++ packages/gatsby-theme-wordpress-basic/src/components/SiteLayout.js
@@ -1,14 +1,20 @@
 import React from "react";
 
+import { usePageContext } from "../hooks";
+
 import Header from "./Header";
 import PageBreadcrumbs from "./PageBreadcrumbs";
+import * as defaultStyles from "./SiteLayout.module.css";
 
-export default function SiteLayout({ children }) {
+export default function SiteLayout({ styles = defaultStyles, children }) {
+  const { contentNode: { isFrontPage } = {} } = usePageContext();
   return (
     <div>
       <Header />
-      <PageBreadcrumbs />
-      <main>{children}</main>
+      <main className={styles.main}>
+        {!isFrontPage && <PageBreadcrumbs className={styles.breadcrumbs} />}
+        {children}
+      </main>
     </div>
   );
 }
