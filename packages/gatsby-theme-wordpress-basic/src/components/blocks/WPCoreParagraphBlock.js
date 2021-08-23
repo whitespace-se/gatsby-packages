f9d2e10b9fff39b260218c65bf024ecbe6479e25
Fredrik Johansson <jeanfredrik@gmail.com>
2021-08-23 16:29:47 +0200

    fix: wp blocks did not handle media embedded correctly

--- packages/gatsby-theme-wordpress-basic/src/components/blocks/WPCoreParagraphBlock.js
+++ packages/gatsby-theme-wordpress-basic/src/components/blocks/WPCoreParagraphBlock.js
@@ -4,6 +4,6 @@ import { useHTMLProcessor } from "../../hooks/html-processor";
 
 export default function WPCoreParagraphBlock({ block, contentMedia }) {
   const { processContent } = useHTMLProcessor();
-  const content = processContent(block.originalContent, contentMedia);
+  const content = processContent(block.originalContent, { contentMedia });
   return <>{content}</>;
 }
