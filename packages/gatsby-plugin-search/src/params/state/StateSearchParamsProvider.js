60571b2e78397969fde4ef91198348b9e94a53d1
Fredrik Johansson <jeanfredrik@gmail.com>
2021-09-20 09:26:09 +0200

    fix: improve `StateSearchParamsProvider`

--- packages/gatsby-plugin-search/src/params/state/StateSearchParamsProvider.js
+++ packages/gatsby-plugin-search/src/params/state/StateSearchParamsProvider.js
@@ -1,22 +1,80 @@
+import { omit } from "lodash-es";
 import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React, { forwardRef, useCallback, useMemo, useState } from "react";
 
 import context from "../context";
 
 StateSearchParamsProvider.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]),
   forcedParams: PropTypes.object,
-  children: PropTypes.node,
+  schema: PropTypes.shape({
+    cast: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
+const toURL = () => null;
+
+const DefaultWrappedLink = forwardRef(function DefaultWrappedLinkWithRef(
+  { ...restProps },
+  ref,
+) {
+  return <button ref={ref} {...restProps} />;
+});
+
 export default function StateSearchParamsProvider({
-  forcedParams = {},
   children,
+  forcedParams = {},
+  schema,
+  Link: WrappedLink = DefaultWrappedLink,
 }) {
   const { Provider } = context;
-  const [params, setParams] = useState({});
+  const [explicitParams, setExcplicitParams] = useState({});
+
+  const params = schema.cast({ ...explicitParams, ...forcedParams });
+
+  const setParams = (modifier) => {
+    setExcplicitParams((params) => {
+      return omit(
+        schema.cast(
+          typeof newParams === "function" ? modifier(params) : modifier,
+        ),
+        Object.keys(forcedParams),
+      );
+    });
+  };
+
+  const Link = useMemo(
+    () =>
+      forwardRef(function LinkWithRef({ to, onClick, ...restProps }, ref) {
+        return (
+          <WrappedLink
+            ref={ref}
+            onClick={(event) => {
+              event.preventDefault();
+              setParams(to);
+              if (onClick) onClick(event);
+            }}
+            {...restProps}
+          />
+        );
+      }),
+    [setParams, WrappedLink],
+  );
+
+  const value = {
+    params,
+    urlParams: explicitParams,
+    explicitParams,
+    forcedParams,
+    setParams,
+    toURL,
+    Link,
+    schema,
+  };
+
   return (
-    <Provider value={[{ ...params, ...forcedParams }, setParams]}>
-      {children}
+    <Provider value={value}>
+      {typeof children === "function" ? children(value) : children}
     </Provider>
   );
 }
