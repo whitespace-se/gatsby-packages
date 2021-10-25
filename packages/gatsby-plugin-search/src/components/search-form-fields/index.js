780b03bfa1c00584ae22edfd2b129f4bdf2d6677
Fredrik Johansson <jeanfredrik@gmail.com>
2021-10-25 10:30:30 +0200

    fix: search sort options missing

--- packages/gatsby-plugin-search/src/components/search-form-fields/index.js
+++ packages/gatsby-plugin-search/src/components/search-form-fields/index.js
@@ -1,4 +1,5 @@
 export { default as SearchFormButtonGroupField } from "./SearchFormButtonGroupField";
+export { default as SearchFormLinksField } from "./SearchFormLinksField";
 export { default as SearchFormMonthSelectField } from "./SearchFormMonthSelectField";
 export { default as SearchFormQueryField } from "./SearchFormQueryField";
 export { default as SearchFormSelectField } from "./SearchFormSelectField";
