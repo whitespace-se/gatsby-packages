b7d55eeb896ee6308b4fc1c2ee32dba7fb6b78f5
Fredrik Johansson <jeanfredrik@gmail.com>
2021-11-09 14:29:14 +0100

    feat: allow setting of `transformProps` via theme

--- packages/gatsby-theme-wordpress-basic/src/components/Archive.js
+++ packages/gatsby-theme-wordpress-basic/src/components/Archive.js
@@ -1,4 +1,5 @@
 import { H, Section } from "@jfrk/react-heading-levels";
+import { withComponentDefaults } from "@whitespace/components";
 import {
   LazyMinisearchSearchBackendProvider,
   SearchContextDebug,
@@ -8,6 +9,7 @@ import {
   SearchPagination,
 } from "@whitespace/gatsby-plugin-search";
 import clsx from "clsx";
+import { flow } from "lodash-es";
 import PropTypes from "prop-types";
 import React from "react";
 
@@ -22,13 +24,17 @@ import { useArchiveParamTypes, usePageContext } from "../hooks";
 import * as defaultStyles from "./Archive.module.css";
 
 Archive.propTypes = {
-  styles: PropTypes.objectOf(PropTypes.string),
   className: PropTypes.string,
+  styles: PropTypes.objectOf(PropTypes.string),
+  transformParams: PropTypes.func,
 };
 
-export default function Archive({
-  styles = defaultStyles,
+export default withComponentDefaults(Archive, "archive");
+
+function Archive({
   className,
+  styles = defaultStyles,
+  transformParams = (params) => params,
   ...restProps
 }) {
   const paramTypes = useArchiveParamTypes();
@@ -67,26 +73,29 @@ export default function Archive({
                   settings={{
                     attributesForFaceting: ["contentType", "tags", "month"],
                   }}
-                  transformParams={({ date, ...params }) => ({
-                    ...params,
-                    ...(/^\d{4}$/.test(date) && {
-                      month: [
-                        "01",
-                        "02",
-                        "03",
-                        "04",
-                        "05",
-                        "06",
-                        "07",
-                        "08",
-                        "09",
-                        "10",
-                        "11",
-                        "12",
-                      ].map((m) => `${date}-${m}`),
+                  transformParams={flow([
+                    transformParams,
+                    ({ date, ...params }) => ({
+                      ...params,
+                      ...(/^\d{4}$/.test(date) && {
+                        month: [
+                          "01",
+                          "02",
+                          "03",
+                          "04",
+                          "05",
+                          "06",
+                          "07",
+                          "08",
+                          "09",
+                          "10",
+                          "11",
+                          "12",
+                        ].map((m) => `${date}-${m}`),
+                      }),
+                      ...(/^\d{4}-\d{2}$/.test(date) && { month: [date] }),
                     }),
-                    ...(/^\d{4}-\d{2}$/.test(date) && { month: [date] }),
-                  })}
+                  ])}
                 >
                   <SearchForm />
                   {process.env.NODE_ENV !== "production" && (
--- packages/gatsby-theme-wordpress-basic/src/components/SiteSearch.js
+++ packages/gatsby-theme-wordpress-basic/src/components/SiteSearch.js
@@ -1,4 +1,5 @@
 import { H } from "@jfrk/react-heading-levels";
+import { withComponentDefaults } from "@whitespace/components";
 import {
   URLSearchParamsProvider,
   LazyMinisearchSearchBackendProvider,
@@ -8,6 +9,8 @@ import {
   SearchPagination,
 } from "@whitespace/gatsby-plugin-search";
 import clsx from "clsx";
+import { flow } from "lodash-es";
+import PropTypes from "prop-types";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
 
@@ -15,9 +18,13 @@ import { useSiteSearchParamTypes } from "../hooks";
 
 import * as searchStyles from "./SiteSearch.module.css";
 
-export default SiteSearch;
+SiteSearch.propTypes = {
+  transformParams: PropTypes.func,
+};
 
-function SiteSearch() {
+export default withComponentDefaults(SiteSearch, "siteSearch");
+
+function SiteSearch({ transformParams = (params) => params }) {
   const { t } = useTranslation();
   const paramTypes = useSiteSearchParamTypes();
   return (
@@ -44,26 +51,29 @@ function SiteSearch() {
               settings={{
                 attributesForFaceting: ["contentType", "tags", "month"],
               }}
-              transformParams={({ date, ...params }) => ({
-                ...params,
-                ...(/^\d{4}$/.test(date) && {
-                  month: [
-                    "01",
-                    "02",
-                    "03",
-                    "04",
-                    "05",
-                    "06",
-                    "07",
-                    "08",
-                    "09",
-                    "10",
-                    "11",
-                    "12",
-                  ].map((m) => `${date}-${m}`),
+              transformParams={flow([
+                transformParams,
+                ({ date, ...params }) => ({
+                  ...params,
+                  ...(/^\d{4}$/.test(date) && {
+                    month: [
+                      "01",
+                      "02",
+                      "03",
+                      "04",
+                      "05",
+                      "06",
+                      "07",
+                      "08",
+                      "09",
+                      "10",
+                      "11",
+                      "12",
+                    ].map((m) => `${date}-${m}`),
+                  }),
+                  ...(/^\d{4}-\d{2}$/.test(date) && { month: [date] }),
                 }),
-                ...(/^\d{4}-\d{2}$/.test(date) && { month: [date] }),
-              })}
+              ])}
             >
               <SearchForm className={searchStyles.form} />
               {process.env.NODE_ENV !== "production" && (
