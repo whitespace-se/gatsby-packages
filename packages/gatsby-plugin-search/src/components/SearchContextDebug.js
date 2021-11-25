103ef43ab673728fed83e227925dbabd4619849f
Fredrik Johansson <jeanfredrik@gmail.com>
2021-11-25 14:18:27 +0100

    fix: Avoid JSON stringify error

--- packages/gatsby-plugin-search/src/components/SearchContextDebug.js
+++ packages/gatsby-plugin-search/src/components/SearchContextDebug.js
@@ -4,9 +4,15 @@ import { useSearch } from "../hooks";
 
 export default function SearchContextDebug({ ...restProps }) {
   const context = useSearch();
+  let output;
+  try {
+    output = JSON.stringify(context, null, 2);
+  } catch {
+    // Do nothing
+  }
   return (
     <pre {...restProps}>
-      <code>{JSON.stringify(context, null, 2)}</code>
+      <code>{output}</code>
     </pre>
   );
 }
