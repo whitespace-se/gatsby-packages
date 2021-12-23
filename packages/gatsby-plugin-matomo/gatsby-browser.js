3addbcf21ebd2b0503ae1f8358208835a10cd7ed
Fredrik Johansson <jeanfredrik@gmail.com>
2021-12-23 16:02:48 +0100

    fix: Error in onRouteUpdate

--- packages/gatsby-plugin-matomo/gatsby-browser.js
+++ packages/gatsby-plugin-matomo/gatsby-browser.js
@@ -1,18 +1,21 @@
-export function onRouteUpdate(_, pluginOptions) {
-  if (
-    process.env.NODE_ENV === `production` ||
-    pluginOptions.includeInDevelopment
-  ) {
+export function onRouteUpdate(
+  _,
+  {
+    includeInDevelopment = false,
+    mtmDataVariableName = "mtm",
+    routeChangeEventName = `gatsby-route-change`,
+  },
+) {
+  if (process.env.NODE_ENV === `production` || includeInDevelopment) {
     // wrap inside a timeout to ensure the title has properly been changed
     setTimeout(() => {
-      const data = pluginOptions.mtmDataVariableName
-        ? window[pluginOptions.mtmDataVariableName]
-        : window.mtmDataVariable;
-      const eventName = pluginOptions.routeChangeEventName
-        ? pluginOptions.routeChangeEventName
+      const data = mtmDataVariableName && window[mtmDataVariableName];
+
+      const eventName = routeChangeEventName
+        ? routeChangeEventName
         : `gatsby-route-change`;
 
-      data?.push({ event: eventName });
+      data.push({ event: eventName });
     }, 50);
   }
 }
