f7025dc95f3734bceba5bff522aeae8ac710e185
Fredrik Johansson <jeanfredrik@gmail.com>
2021-12-23 16:02:29 +0100

    feat: add export `truey` and `falsey`

--- packages/gatsby-theme-wordpress-basic/index.js
+++ packages/gatsby-theme-wordpress-basic/index.js
@@ -1,1 +1,17 @@
 module.exports = require("./node/config");
+
+module.exports.falsey = function falsey(value) {
+  try {
+    return !JSON.parse(value);
+  } catch {
+    return !value;
+  }
+};
+
+module.exports.truey = function truey(value) {
+  try {
+    return !!JSON.parse(value);
+  } catch {
+    return !!value;
+  }
+};
