d268abcbecb6b231db5de9042e994c7dacb4631b
Fredrik Johansson <jeanfredrik@gmail.com>
2022-03-15 10:11:15 +0100

    Revert "Remove: mtmDDV, mtmDVN, mtmPAQDDV, mtmPAQDVN"
    This reverts commit ed6d967746057a9fed4999cab7716c124630ad61.

--- packages/gatsby-plugin-matomo/gatsby-node.js
+++ packages/gatsby-plugin-matomo/gatsby-node.js
@@ -20,6 +20,20 @@ exports.pluginOptionsSchema = ({ Joi }) =>
     includeInDevelopment: Joi.boolean()
       .default(false)
       .description(`Include Matomo plugin when running in development mode.`),
+    mtmDefaultDataVariable: Joi.alternatives()
+      .try(Joi.object(), Joi.function())
+      .default(null)
+      .description(
+        `Data variable to be set before Matomo plugin is loaded. Should be an object or a function.`,
+      ),
+    mtmDataVariableName: Joi.string().description(`Data variable name.`),
+    mtmPAQDefaultDataVariable: Joi.alternatives()
+      .try(Joi.object(), Joi.function())
+      .default(null)
+      .description(
+        `Data variable for PAQ to be set before Matomo plugin is loaded. Should be an object or a function.`,
+      ),
+    mtmPAQDataVariableName: Joi.string().description(`PAQ Data variable name.`),
     routeChangeEventName: Joi.alternatives()
       .try(Joi.string(), Joi.boolean().equal(false))
       .default(`gatsby-route-change`)
--- packages/gatsby-plugin-matomo/gatsby-ssr.js
+++ packages/gatsby-plugin-matomo/gatsby-ssr.js
@@ -1,27 +1,84 @@
 import { oneLine } from "common-tags";
 import React from "react";
 
-const generateMTM = ({ mtmContainerId, mtmHost }) => oneLine`
-  var _mtm = window._mtm = window._mtm || [];
-  var _paq = window._paq = window._paq || [];
+const generateMTM = ({
+  mtmContainerId,
+  mtmHost,
+  mtmDataVariableName,
+}) => oneLine`
+  var _mtm = window.${mtmDataVariableName} = window.${mtmDataVariableName} || [];
   _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtmStart'});
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.async=true; g.src='${mtmHost}/js/${mtmContainerId}.js'; s.parentNode.insertBefore(g,s);
 `;
 
+const generateMTMPAQ = ({ mtmPAQDataVariableName }) => oneLine`
+  var _paq = window.${mtmPAQDataVariableName} = window.${mtmPAQDataVariableName} || [];
+`;
+
 const generateMTMframe = ({ mtmContainerId, mtmHost }) =>
   oneLine`<iframe src="${mtmHost}/js/${mtmContainerId}.js" height="0" width="0" style="display: none; visibility: hidden" aria-hidden="true"></iframe>`;
+
+const generateDefaultDataVariable = (
+  mtmDataVariable,
+  reporter,
+  mtmDataVariableName,
+) => {
+  let result = `window.${mtmDataVariableName} = window.${mtmDataVariableName} || [];`;
+
+  if (mtmDataVariable.type === `function`) {
+    result += `window.${mtmDataVariableName}.push((${mtmDataVariable.value})());`;
+  } else {
+    if (
+      mtmDataVariable.type !== `object` ||
+      mtmDataVariable.value.constructor !== Object
+    ) {
+      reporter.panic(
+        `Oops the plugin option "default mtmDataVariable" should be a plain object. "${mtmDataVariable}" is not valid.`,
+      );
+    }
+
+    result += `window.${mtmDataVariableName}.push(${JSON.stringify(
+      mtmDataVariable.value,
+    )});`;
+  }
+
+  return oneLine`${result}`;
+};
 
 export const onRenderBody = (
   { setHeadComponents, setPreBodyComponents, reporter },
   {
     includeInDevelopment = false,
     mtmContainerId,
+    mtmDataVariableName = `mtm`,
+    mtmDefaultDataVariable,
     mtmHost = `https://www.matomo.com`,
+    mtmPAQDataVariableName = `paq`,
+    mtmPAQDefaultDataVariable,
     requireCookieConsent = false,
   },
 ) => {
   if (process.env.NODE_ENV === `production` || includeInDevelopment) {
+    let defaultMTMCode = ``;
+    let defaultMTMPAQCode = ``;
+
+    if (mtmDefaultDataVariable) {
+      defaultMTMCode = generateDefaultDataVariable(
+        mtmDefaultDataVariable,
+        reporter,
+        mtmDataVariableName,
+      );
+    }
+
+    if (mtmPAQDefaultDataVariable) {
+      defaultMTMPAQCode = generateDefaultDataVariable(
+        mtmPAQDefaultDataVariable,
+        reporter,
+        mtmPAQDataVariableName,
+      );
+    }
+
     mtmHost = mtmHost.replace(/\/$/, ``);
 
     const inlineScripts = [];
@@ -31,25 +88,45 @@ export const onRenderBody = (
         key="plugin-matomo-mtm"
         dangerouslySetInnerHTML={{
           __html: oneLine`
+          ${defaultMTMCode}
           ${generateMTM({
             mtmContainerId,
             mtmHost,
-          })}
-          ${requireCookieConsent ? `_paq.push(['requireCookieConsent']);` : ""}
-          `,
+            mtmDataVariableName,
+          })}`,
         }}
       />,
     );
+
+    if (mtmPAQDataVariableName) {
+      inlineScripts.push(
+        <script
+          key="plugin-matomo-mtm-paq"
+          dangerouslySetInnerHTML={{
+            __html: oneLine`
+            ${defaultMTMPAQCode}
+            ${generateMTMPAQ({
+              mtmPAQDataVariableName,
+            })}
+            ${
+              requireCookieConsent ? `_paq.push(['requireCookieConsent']);` : ""
+            }
+            `,
+          }}
+        />,
+      );
+    }
 
     setHeadComponents(inlineScripts);
 
     setPreBodyComponents([
       <noscript
-        key="plugin-matomo-mtm-noscript"
+        key="plugin-matomo-mtm"
         dangerouslySetInnerHTML={{
           __html: generateMTMframe({
             mtmContainerId,
             mtmHost,
+            mtmDataVariableName,
           }),
         }}
       />,
