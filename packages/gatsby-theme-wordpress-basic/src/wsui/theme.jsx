bdd9cf594d2929def1ffa1a95109c0ad0ae31ea3
Fredrik Johansson <jeanfredrik@gmail.com>
2023-03-30 16:30:45 +0200

    feat: Set default grid item width

--- packages/gatsby-theme-wordpress-basic/src/wsui/theme.jsx
+++ packages/gatsby-theme-wordpress-basic/src/wsui/theme.jsx
@@ -2,4 +2,11 @@ import { createTheme } from "@wsui/base";
 
 export default createTheme({
   colors: {},
+  components: {
+    PageGrid: {
+      defaultProps: {
+        maxColspan: 7,
+      },
+    },
+  },
 });
