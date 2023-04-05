f33d5f3d9b6923a9e8148533a04ced099e4c69fc
Fredrik Johansson <jeanfredrik@gmail.com>
2023-04-05 09:13:12 +0200

    fix: SSR issues

--- packages/gatsby-theme-wordpress-basic/src/wsui/components/HeaderHamburgerMenu.jsx
+++ packages/gatsby-theme-wordpress-basic/src/wsui/components/HeaderHamburgerMenu.jsx
@@ -1,31 +1,19 @@
 /** @jsx jsx */
-import { jsx, css } from "@emotion/react";
-import { Button, SlidePanel } from "@wsui/base";
-import { Fragment, Suspense, lazy, useState, useEffect } from "react";
+import { jsx } from "@emotion/react";
+import { IsomorphicSuspense, SlidePanel } from "@wsui/base";
+import { lazy } from "react";
 
 const HeaderHamburgerMenuPanel = lazy(async () => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   return import("./HeaderHamburgerMenuPanel.jsx");
 });
 
-function LoadHandler({ onMount, onUnmount }) {
-  useEffect(() => {
-    console.log("loading…");
-    onMount();
-    return () => {
-      console.log("done loading");
-      onUnmount();
-    };
-  }, []);
-  return "Laddar";
-}
-
 export default function HeaderHamburgerMenu({ id, ...restProps }) {
   return (
     <SlidePanel title="Menu" id={id} {...restProps}>
-      <Suspense fallback={<div>Loading…</div>}>
+      <IsomorphicSuspense fallback={<div>Loading…</div>}>
         <HeaderHamburgerMenuPanel />
-      </Suspense>
+      </IsomorphicSuspense>
     </SlidePanel>
   );
 }
