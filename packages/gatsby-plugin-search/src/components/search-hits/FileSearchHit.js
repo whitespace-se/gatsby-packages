53b2bc42ca9902f938230d9018142c53f798d9c4
Daniel Hellström <daniel.hellstrom@whitespace.se>
2023-04-13 13:48:01 +0200

    feat: Add search hit type for files (#32)

---
+++ packages/gatsby-plugin-search/src/components/search-hits/FileSearchHit.js
@@ -0,0 +1,71 @@
+import { Time } from "@whitespace/gatsby-theme-wordpress-basic/src/components";
+import mime from "mime/lite";
+import PropTypes from "prop-types";
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+import SearchTeaser from "../SearchTeaser";
+import SearchTeaserContent from "../SearchTeaserContent";
+import SearchTeaserExcerpt from "../SearchTeaserExcerpt";
+import SearchTeaserMedia from "../SearchTeaserMedia";
+import SearchTeaserMeta from "../SearchTeaserMeta";
+import SearchTeaserTitle from "../SearchTeaserTitle";
+
+FileSearchHit.propTypes = {
+  dateFormat: PropTypes.objectOf(PropTypes.string),
+  hit: PropTypes.shape({
+    date: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)]),
+    text: PropTypes.node,
+    image: PropTypes.object,
+    label: PropTypes.node,
+    // taxonomies: PropTypes.arrayOf(PropTypes.object),
+  }),
+};
+
+export default function FileSearchHit({
+  dateFormat = {
+    dateStyle: "short",
+  },
+  hit,
+  ...restProps
+}) {
+  const { t, i18n } = useTranslation();
+  const {
+    date,
+    text,
+    image,
+    label,
+    file,
+    // taxonomies,
+  } = hit;
+
+  return (
+    <SearchTeaser as="li" {...restProps}>
+      <SearchTeaserContent>
+        <SearchTeaserTitle link={{ url: file.url, target: "_blank" }}>
+          {label}
+        </SearchTeaserTitle>
+        {text && <SearchTeaserExcerpt>{text}</SearchTeaserExcerpt>}
+        <SearchTeaserMeta>
+          <span>
+            {!!file.attachment?.content_type &&
+              t("fileType", {
+                extension: mime
+                  .getExtension(file.attachment.content_type)
+                  .toLocaleUpperCase(i18n.language),
+              })}
+          </span>
+          {date && <Time date={date} format={dateFormat} />}
+        </SearchTeaserMeta>
+      </SearchTeaserContent>
+      {image && (
+        <SearchTeaserMedia
+          image={{
+            ...image,
+            aspectRatio: 155 / 80,
+          }}
+        />
+      )}
+    </SearchTeaser>
+  );
+}
--- packages/gatsby-plugin-search/src/components/search-hits/index.js
+++ packages/gatsby-plugin-search/src/components/search-hits/index.js
@@ -1,2 +1,3 @@
 export { default as ContactSearchHit } from "./ContactSearchHit";
+export { default as FileSearchHit } from "./FileSearchHit";
 export { default as DefaultSearchHit } from "./DefaultSearchHit";
