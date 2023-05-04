96f41e71b2fa22f6520c416818d8d4ed4bcbfaa6
Daniel Hellström <daniel.hellstrom@whitespace.se>
2023-05-04 16:46:46 +0200

    feat: Merge stringifierComponents in HtmlProcessorExtensionProvider

--- packages/gatsby-theme-wordpress-basic/src/components/HtmlProcessorExtensionProvider.js
+++ packages/gatsby-theme-wordpress-basic/src/components/HtmlProcessorExtensionProvider.js
@@ -2,7 +2,7 @@ import { mergeWith } from "lodash/fp";
 import React, { useContext } from "react";
 
 const mergeAndConcat = mergeWith((a, b) =>
-  Array.isArray(a) ? a.concat(b) : b,
+  Array.isArray(a) ? a.concat(b) : typeof a === "object" && a !== null ? {...a, ...b} : b,
 );
 
 import htmlProcessorExtensionContext from "../contexts/htmlProcessorExtensionContext";
