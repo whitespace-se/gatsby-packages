b5441852187b575bf17cbfedcdf6d16ac6952966
Fredrik Johansson <jeanfredrik@gmail.com>
2023-06-08 17:39:57 +0200

    chore: Update deps

--- packages/gatsby-theme-wordpress-basic/src/wsui/theme.jsx
+++ packages/gatsby-theme-wordpress-basic/src/wsui/theme.jsx
@@ -1,11 +1,18 @@
 import { createTheme } from "@wsui/base";
 
+import { Image } from "./components";
+
 export default createTheme({
   colors: {},
   components: {
     PageGrid: {
       defaultProps: {
         maxColspan: 7,
+      },
+    },
+    Image: {
+      defaultProps: {
+        handler: Image,
       },
     },
   },
