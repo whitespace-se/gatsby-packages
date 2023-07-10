c39938a754a908f44b4ebd6e1ca619f8f0753ef7
Fredrik Johansson <jeanfredrik@gmail.com>
2023-07-10 16:20:57 +0200

    feat: Improve PageBreadcrumbs component

--- packages/gatsby-theme-wordpress-basic/src/wsui/components/PageBreadcrumbs.jsx
+++ packages/gatsby-theme-wordpress-basic/src/wsui/components/PageBreadcrumbs.jsx
@@ -1,11 +1,26 @@
 import usePageBreadcrumbs from "@whitespace/gatsby-plugin-breadcrumbs/src/usePageBreadcrumbs";
-import { Breadcrumbs } from "@wsui/base";
+import {
+  Breadcrumbs,
+  PageGrid,
+  PageGridItem,
+  PageSection,
+  useThemeProps,
+} from "@wsui/base";
 import React from "react";
 
-export default function PageBreadcrumbs({
-  transformItems = (items) => items,
-  ...restProps
-}) {
+export default function PageBreadcrumbs(props) {
+  props = useThemeProps({ props, name: "PageBreadcrumbs" });
+  let { transformItems = (items) => items, ...restProps } = props;
   let { items } = usePageBreadcrumbs({ transformItems });
-  return <Breadcrumbs items={items} {...restProps} />;
+  if (!items || items.length < 2) return null;
+
+  return (
+    <PageSection background="transparent" spacing={[2.5, 5]}>
+      <PageGrid>
+        <PageGridItem colspan={12}>
+          <Breadcrumbs items={items} {...restProps} />
+        </PageGridItem>
+      </PageGrid>
+    </PageSection>
+  );
 }
