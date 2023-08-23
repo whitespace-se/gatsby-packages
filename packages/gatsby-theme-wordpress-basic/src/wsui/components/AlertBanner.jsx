5f83117c7ac0ead0365f8a5a1cf7589c6afdb934
Fredrik Johansson <jeanfredrik@gmail.com>
2023-08-23 18:48:27 +0200

    feat: Handle excerpt and read more in alert banner

--- packages/gatsby-theme-wordpress-basic/src/wsui/components/AlertBanner.jsx
+++ packages/gatsby-theme-wordpress-basic/src/wsui/components/AlertBanner.jsx
@@ -5,14 +5,56 @@ import {
   MaybeFragment,
   PageGrid,
   PageGridItem,
-  TypographyBlock,
+  Typography,
+  useThemeProps,
+  Button,
+  handleComponentsProp,
 } from "@wsui/base";
 import { graphql, useStaticQuery } from "gatsby";
-import { useState } from "react";
+import { Fragment, useState } from "react";
+import { useTranslation } from "react-i18next";
 
-import Html from "./Html.jsx";
+import { useHTMLProcessor } from "../../hooks/html-processor.js";
 
-export default function AlertBanner({ ...restProps }) {
+function DefaultReadMore({
+  ownerState: {
+    alertOwnerState: { color },
+  },
+  children,
+  ...restProps
+}) {
+  return (
+    <Fragment>
+      {" "}
+      <Button
+        color={[color, "+100"]}
+        style={{
+          paddingBlock: 2,
+          paddingInline: 3,
+          marginBlock: -2,
+          marginInlineStart: 2,
+        }}
+        variant={null}
+        {...restProps}
+      >
+        {children}
+      </Button>
+    </Fragment>
+  );
+}
+
+export default function AlertBanner(props) {
+  props = useThemeProps({ props, name: "AlertBanner" });
+  const { t } = useTranslation();
+  let {
+    components,
+    readMoreText = t(["alertBanner.readMore", "readMore", "Read more…"]),
+    ...restProps
+  } = props;
+  let { ReadMore } = handleComponentsProp(components, {
+    ReadMore: DefaultReadMore,
+  });
+  const { getPlainTextExcerpt } = useHTMLProcessor();
   const [dismissedBannerIds, setDismissedBannerIds] = useState([]);
 
   let data = useStaticQuery(graphql`
@@ -21,8 +63,7 @@ export default function AlertBanner({ ...restProps }) {
         alerts(first: 1000) {
           nodes {
             id
-            title
-            content
+            ...WP_AlertForBanner
           }
         }
       }
@@ -39,25 +80,29 @@ export default function AlertBanner({ ...restProps }) {
   }
 
   function checkForDismissedBanner(id) {
-    return dismissedBannerIds?.find((item) => item === id);
+    return dismissedBannerIds.find((item) => item === id);
   }
 
+  const ownerState = {
+    ...restProps,
+  };
+
   return (
-    <MaybeFragment {...restProps}>
+    <MaybeFragment>
       {alerts
         .filter(({ id }) => !checkForDismissedBanner(id))
-        .map(({ id, title, content }) => {
+        .map(({ id, excerpt, content, hasPageContent, uri }) => {
+          let plainTextExcerpt = getPlainTextExcerpt({ excerpt, content });
+          if (!plainTextExcerpt) return null;
           return (
             <Alert
               key={id}
-              // css={{
-              //   background: `var(--color-${alertSettingsType})`,
-              //   color: `var(--color-${alertSettingsType}-text)`,
-              // }}
-              title={title}
-              onClose={() => {
-                dismissAlertBanner(id);
-              }}
+              onClose={
+                !!id &&
+                (() => {
+                  dismissAlertBanner(id);
+                })
+              }
               borderRadius={0}
               components={{
                 Inner: ({ children, ...restProps }) => (
@@ -69,11 +114,24 @@ export default function AlertBanner({ ...restProps }) {
               style={{
                 paddingInline: 0,
               }}
+              {...restProps}
             >
-              {/* <Icon name={alertSettingsType} /> */}
-              <TypographyBlock>
-                <Html>{content}</Html>
-              </TypographyBlock>
+              {(alertOwnerState) => (
+                <Typography>
+                  {plainTextExcerpt}
+                  {!!(hasPageContent && uri) && (
+                    <ReadMore
+                      ownerState={{
+                        ...ownerState,
+                        alertOwnerState,
+                      }}
+                      uri={uri}
+                    >
+                      {readMoreText}
+                    </ReadMore>
+                  )}
+                </Typography>
+              )}
             </Alert>
           );
         })}
