db5a35d0caf27826f7213d6ff7749622d7b9fb36
Fredrik Johansson <jeanfredrik@gmail.com>
2023-09-04 13:58:30 +0200

    feat: Add `background` and `spacingStart` props to PageBreadcrumbs

--- packages/gatsby-theme-wordpress-basic/src/wsui/components/PageBreadcrumbs.jsx
+++ packages/gatsby-theme-wordpress-basic/src/wsui/components/PageBreadcrumbs.jsx
@@ -10,12 +10,17 @@ import React from "react";
 
 export default function PageBreadcrumbs(props) {
   props = useThemeProps({ props, name: "PageBreadcrumbs" });
-  let { transformItems = (items) => items, ...restProps } = props;
+  let {
+    transformItems = (items) => items,
+    background = "transparent",
+    spacingStart = [5, 10],
+    ...restProps
+  } = props;
   let { items } = usePageBreadcrumbs({ transformItems });
   if (!items || items.length < 2) return null;
 
   return (
-    <PageSection background="transparent" spacing={[2.5, 5]}>
+    <PageSection background={background} spacingStart={spacingStart}>
       <PageGrid>
         <PageGridItem colspan={12}>
           <Breadcrumbs items={items} {...restProps} />
